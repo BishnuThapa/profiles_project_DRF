945bd2022ceb1c295803c13fadaac8e4fb235b42
Bishnu <thapabishnu20@gmail.com>
2023-05-17 13:16:39 +0545

    custom user model for creting user & superuser

--- profile_api/models.py
+++ profile_api/models.py
@@ -1,3 +1,51 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 # Create your models here.
+
+
+class UserProfileManager(BaseUserManager):
+    """ Manager for user profiles"""
+
+    def create_user(self, email, name, password=None):
+        """Create a new user profile"""
+        if not email:
+            raise ValueError('User must have an email address.')
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+
+        # set password for hashing password
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, name, password):
+        """cerate and save new superuser"""
+        user = self.create_user(email, name, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Database models for users in the system """  # python standard for writting docstring
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    # custom user manager for registering model
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'  # for login
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        return self.name
+
+    def get_short_name(self):
+        return self.name
+
+    def __str__(self):
+        """ Return string representation of our user"""
+        return self.email
