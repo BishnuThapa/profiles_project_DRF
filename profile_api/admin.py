f5ba59c7f47ed0cef653d77decfe9a5d07b0b1c8
Bishnu <thapabishnu20@gmail.com>
2023-05-17 13:24:05 +0545

    password readonly in admin panel

--- profile_api/admin.py
+++ profile_api/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
+from .models import *
 # Register your models here.
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(UserAdmin):
+    list_display = ('email', 'name',
+                    'is_active')
+    list_display_links = ('email', 'name',)
+    ordering = ('-email',)
+    filter_horizontal = ()
+    fieldsets = ()
+    list_filter = ()
