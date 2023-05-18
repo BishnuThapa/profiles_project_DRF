d86182b98e5654c913ff8a949f5f15554c093a2b
Bishnu <thapabishnu20@gmail.com>
2023-05-18 12:19:56 +0545

    added viewset to profiles api

--- profile_api/views.py
+++ profile_api/views.py
@@ -1,9 +1,38 @@
 from django.shortcuts import render
 from rest_framework import status
 from rest_framework.views import APIView
+from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from .serializers import HelloSerializer
 # Create your views here.
+
+class HelloViewSet(ViewSet):
+    serializer_class=HelloSerializer
+    """ Test API Viewset"""
+
+    def list(self,request):
+        return Response({'message':'Hello'})
+    
+    def create(self,request):
+        serializer=self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name=serializer.validated_data.get('name')
+            message=f'Hello {name}'
+            return Response({'message':message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    
+    def retrieve(self,request,pk=None):
+        return Response({'Method':'GET'})
+    
+    def update(self,request,pk=None):
+        return Response({'Method':'PUT'})
+    def partial_update(self,request,pk=None):
+        return Response({'Method':'PATCH'})
+
+    def destroy(self,request,pk=None):
+        return Response({'Method':'DESTROY'})
 
 
 class HelloApiView(APIView):
